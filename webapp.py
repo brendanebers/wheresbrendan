4a85981c2620da21765cd09c661f73b675acd239
Brendan Ebers <brendan.ebers@gmail.com>
2015-07-07 21:22:11 -0500

    webapp module to appease gunicorn

---
+++ webapp.py
@@ -0,0 +1,6 @@
+"""This module is used by gunicorn to launch the web app."""
+from app import flask_app  # The app must be imported first.
+from app import views  # This registers the views with the flask app
+
+# A necessary evil; gunicorn needs need a Flask object within the module.
+app = flask_app.flask_app
