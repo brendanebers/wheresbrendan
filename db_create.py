3799425ae0e0c9f386653b80dbe5da18d99ac769
Brendan Ebers <brendan.ebers@gmail.com>
2015-07-08 16:14:18 -0500

    "Finalized" the database scripts

--- db_create.py
+++ db_create.py
@@ -1,17 +1,18 @@
 #!flask/bin/python
 """TODO: Document / clean this up."""
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from app import config import SQLALCHEMY_MIGRATE_REPO
-from app.flask_app import db
-from app.flask_app import models  # Just making sure they're loaded.
 import os
 
-if not os.path.exists(config.SQLALCHEMY_MIGRATE_REPO):
-    api.create(config.SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(config.SQLALCHEMY_DATABASE_URI,
-        config.SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(config.SQLALCHEMY_DATABASE_URI,
-        config.SQLALCHEMY_MIGRATE_REPO,
-        api.version(config.SQLALCHEMY_MIGRATE_REPO))
+import config
+from app.flask_app import db
+from app import models  # Just making sure they're loaded.
+
+if __name__ == '__main__':
+    if not os.path.exists(config.SQLALCHEMY_MIGRATE_REPO):
+        api.create(config.SQLALCHEMY_MIGRATE_REPO, 'database repository')
+        api.version_control(config.SQLALCHEMY_DATABASE_URI,
+            config.SQLALCHEMY_MIGRATE_REPO)
+    else:
+        api.version_control(config.SQLALCHEMY_DATABASE_URI,
+            config.SQLALCHEMY_MIGRATE_REPO,
+            api.version(config.SQLALCHEMY_MIGRATE_REPO))
--- db_migrate.py
+++ db_migrate.py
@@ -3,7 +3,7 @@
 import imp
 from migrate.versioning import api
 from app.flask_app import db
-from app.flask_app import models  # Just making sure they're loaded.
+from app import models  # Just making sure they're loaded.
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
@@ -12,7 +12,10 @@ tmp_module = imp.new_module('old_model')
 old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 exec(old_model, tmp_module.__dict__)
 script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
-open(migration, "wt").write(script)
+fh = open(migration, "wt")
+fh.write('from sqlalchemy.dialects.postgresql import *\n')
+fh.write(script)
+fh.close()
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print('New migration saved as ' + migration)
