105c1b0e1bf56955ace97460e57583c367aed678
Sean Laurent <organicveggie@gmail.com>
2015-07-21 07:27:25 -0700

    Linter cleanup for Flake8.

--- db_migrate_models.py
+++ db_migrate_models.py
@@ -6,7 +6,7 @@ import os
 
 from migrate.versioning import api
 from app.flask_app import db
-from app import models  # Just making sure they're loaded.
+from app import models  # Just making sure they're loaded. # flake8: noqa
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 
@@ -15,7 +15,7 @@ def main():
     print "Reading database version..."
     current_version = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     next_version = current_version + 1
-    
+
     # Generate name for next migration script
     new_migration_name = '%03d_migration.py' % next_version
     migration = os.path.join(SQLALCHEMY_MIGRATE_REPO, 'versions', new_migration_name)
@@ -24,7 +24,8 @@ def main():
     old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     exec(old_model, tmp_module.__dict__)
 
-    script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
+    script = api.make_update_script_for_model(
+        SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
     fh = open(migration, 'wt')
     fh.write('from sqlalchemy.dialects.postgresql import *\n')
     fh.write(script)
--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 """This module is used by gunicorn to launch the web app."""
 from app import flask_app  # The app must be imported first.
-from app import views  # This registers the views with the flask app
+from app import views  # This registers the views with the flask app # flake8: noqa
 
 # A necessary evil; gunicorn needs need a Flask object within the module.
 app = flask_app.flask_app
