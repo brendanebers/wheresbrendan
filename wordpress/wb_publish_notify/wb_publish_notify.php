874a9de84ae7986af62487fc3ae5d8c5595ac0e4
Brendan Ebers <brendan.ebers@gmail.com>
2015-07-28 21:49:39 -0500

    My first wordpress plugin! It sends the ID to a URL when a post has been updated.

---
+++ wordpress/wb_publish_notify/wb_publish_notify.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Where's Brendan Post Notifier
+Plugin URI: http://wheresbrendan.com
+Description: A plugin that pings wheresbrendan whenever a post is generated.
+Version: 0.1
+Author: Brendan Ebers
+Author URI: http://brendanebers.com
+*/
+
+add_action( 'publish_post', 'published_notification', 10, 2 );
+
+function published_notification( $ID, $post ) {
+  // TODO: Point this at the actual site sometime.
+  // POST is probably smarter.
+  $url = 'http://runbebersrun.herokuapp.com/api/wordpress/' . $ID;
+
+  try {
+    file_get_contents($url);
+  }
+  catch (Exception $e) {
+    // Look at me not caring!
+  }
+}
+?>
