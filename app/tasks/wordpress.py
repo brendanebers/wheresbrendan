6a45035a761365f924b3bf8e0a6da30011aa4c22
Brendan Ebers <brendan.ebers@gmail.com>
2015-08-06 18:00:46 -0500

    Added some basic model related functions to the wordpress module #4

--- app/tasks/wordpress.py
+++ app/tasks/wordpress.py
@@ -4,16 +4,32 @@
 # to our API when a page is changed...
 # https://codex.wordpress.org/Plugin_API/Action_Reference/save_post
 
-# https://python-wordpress-xmlrpc.readthedocs.org
-import wordpress_xmlrpc
+import datetime
+import wordpress_xmlrpc  # https://python-wordpress-xmlrpc.readthedocs.org
 from wordpress_xmlrpc.methods import posts as wp_posts
 
 import config
+from app import models
 
 
 def _Client():
     return wordpress_xmlrpc.Client(
         config.WORDPRESS_SITE, config.WORDPRESS_USER, config.WORDPRESS_PASSWORD)
+
+
+def _ToEpoch(dt):
+    """Return epoch from wordpress's peculiar DateTime object."""
+    tt = dt.timetuple()
+    return datetime.datetime(
+        year=tt.tm_year, month=tt.tm_mon, day=tt.tm_mday, hour=tt.tm_hour,
+        minute=tt.tm_min, second=tt.tm_sec)
+
+
+def _ToPostModel(post):
+    post_ts = _ToEpoch(post.struct['post_date_gmt'])
+    return models.Post(
+        post_id=post.id, title=post.title, status=post.status,
+        content=post.content, link=post.link, timestamp=post_ts)
 
 
 def GetPosts():
@@ -22,3 +38,23 @@ def GetPosts():
     post_query = wp_posts.GetPosts({'post_status': 'publish'})
     posts = client.call(post_query)
     return posts
+
+
+def GetPost(post_id):
+    """Return a given post by ID."""
+    client = _Client()
+    post_query = wp_posts.GetPosts({'post_id': post_id})
+    posts = client.call(post_query)
+    return posts[0] if posts else None
+
+
+def UpdatePosts():
+    """Ensure all posts in the database are up to date."""
+    posts = GetPosts()
+    posts = [_ToPostModel(post) for post in posts]
+
+
+def UpdatePost(post_id):
+    """Ensure given post id is up to date in the database."""
+    post = GetPost(post_id)
+    post = _ToPostModel(post)
