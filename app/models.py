1745c9de51c941bd3df1fbc7e01f302df86d77eb
Brendan Ebers <brendan.ebers@gmail.com>
2015-08-06 18:11:10 -0500

    Method to save and update wordpress posts in the database #4

--- app/models.py
+++ app/models.py
@@ -51,7 +51,7 @@ class Post(db.Model):
 
     """A blog post."""
 
-    id = db.Column(db.Integery, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
     # Wordpress stuff.
     post_id = db.Column(db.Integer, unique=True)
@@ -110,6 +110,31 @@ def _IsField(name, only, skip):
         return name in only
     invalid_fields = ['query', 'metadata', 'query_class'] + skip
     return name[0].islower() and name not in invalid_fields
+
+
+def SavePosts(posts):
+    """Save new posts and update existing posts."""
+    old_posts = {}
+    for post in posts:
+        old = Post.query.filter(Post.post_id == post.post_id).first()
+        old_posts[post.post_id] = old
+
+    for post in posts:
+        if post.post_id not in old_posts:
+            db.session.add(post)
+        else:
+            old = old_posts[post.post_id]
+            old.post_id = post.post_id
+            old.status = post.status
+            old.title = post.title
+            old.content = post.content
+            old.timestamp = post.timestamp
+            old.link = post.link
+            old.location = post.location
+            old.latitude = post.latitude
+            old.longitude = post.longitude
+    db.session.commit()
+
 
 
 def AsDict(obj, only=None, skip=None):
