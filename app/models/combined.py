f405118c97483c81c1a14f36a067245883caec30
Brendan Ebers <brendan.ebers@gmail.com>
2015-08-07 18:06:56 -0500

    Added a method for tagging moving stops.

--- app/models/combined.py
+++ app/models/combined.py
@@ -5,11 +5,30 @@ from app.models import post as post_model
 from app.models import utils
 
 
+def TagShortStops(positions):
+    """Tag short stops with ['icon'] = 'position'."""
+    previous = None
+    for position in positions:
+        if not previous:
+            previous = position
+            continue
+
+        # This is a cheat, we need to use tags that I'm not saving yet.
+        if (position['epoch'] - previous['epoch']) > 60*60:  # More than an hour
+            previous = position
+            continue
+
+        previous['icon'] = 'position'
+        previous = position
+
+
 def Range(start=None, end=None):
     """Return combined position and post dictionaries for given range."""
     positions = position_model.PositionRange(start=start, end=end)
-    positions = utils.RowsAsDicts(positions)  # TODO: Update skip fields
-    # TODO: Positions that are just movement should be a little icon.
+    positions = utils.RowsAsDicts(positions)
+    positions.sort(key=lambda p: p['epoch'])
+    TagShortStops(positions)
+    # TODO: Update skip fields
 
     posts = post_model.PostRange(start=start, end=end)
     # TODO: Update skip fields
