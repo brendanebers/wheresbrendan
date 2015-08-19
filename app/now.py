0ce5ff498adc83f0d2671b27546047c5c2687fd7
Brendan Ebers <brendan.ebers@gmail.com>
2015-08-18 19:05:09 -0500

    Simplified time since results. "There for 13 days, 2 hours" is now "There for 13 days"

--- app/now.py
+++ app/now.py
@@ -18,16 +18,8 @@ def HumanizeSeconds(delta_seconds):
     """Return a human friendly string of how much time has elapsed."""
     relative = relativedelta.relativedelta(seconds=delta_seconds)
     attrs = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
-    result = []
-    last = False
     for attr in attrs:
         val = getattr(relative, attr)
         if val:
-            result.append('%d %s' % (val, attr))
-            if val == 1:  # Strip off the s for singular.
-                result[-1] = result[-1][:-1]
-            if last:
-                break
-            else:
-                last = True
-    return ', '.join(result)
+            return '%d %s' % (val, attr)
+    return 'NO TIME AT ALL'
